041e0453d91a8c7606a52ea4c3ef90b85463b229
E-Martynenko1982 <84075901+E-Martynenko1982@users.noreply.github.com>
2022-03-12 17:31:23 +0200

    new resume

---
+++ js/main.js
@@ -0,0 +1,135 @@
+var arrLang = {
+  'en': {
+    'name': 'Martynenko Evgeniy',
+    'experience': `active sales of non-industrial group,
+               appraisal of mortgaged property (pawnshop),
+               own business (purchase, sale of equipment for heavy industry).`,
+    'exp': 'Experience:',
+    'des': 'Skills:',
+    'ed': 'Education:',
+    'edvalue': 'Banking. Bachelor. Ukrainian Academy of Banking.',
+    'loc': 'Cherkassy, Ukraine',
+    'location': 'Location',
+    'courses': 'Courses',
+    'phone': 'Phone:',
+    'letter': 'Cover letter',
+    'coletter': `Hello, my name is Eugene, I am 39 years old.
+      I firmly decided to change the nature of my activity, as I consider the field of development in IT to be very attractive and interesting.
+      I have come a long way in life and worked in different fields.In some I started from scratch without understanding, for example
+      how an elevator or an excavator or an overhead crane or an industrial fire pump is arranged and what can unite them.
+      But due to the fact that I was interested in it and I enjoyed the fact that I solved seemingly insoluble problems -
+    I have been successful.
+      I even managed to organize my own small business in which I was both a director and a sales manager and an organizer
+      logistics
+      and an accountant and a driver with a loader in one person.Having reached a certain level, I realized that I had adjusted everything so much that
+      no longer interested
+      work because there were no new tasks to solve.Everything went smoothly, there was no such excitement.
+      Why did I choose IT?
+      Because I really look at things and understand that this is the future.
+      I tried it and I liked it, and again there was a passion that has not let me go for a year.
+      It was hard for me to study and work at two jobs, and in recent days also in a position where there is a war in my country.
+      But I don't give up. I'm 39 but I'm 20 at heart. I play sports and lead a healthy lifestyle. I am full of energy and optimism.
+      I am engaged in spiritual practices in order to become better and more useful for this world.
+    Honesty, sanity, tolerance, communication skills, the ability to wait, the ability to adapt - these are the qualities for which
+      I am appreciated by my colleagues
+      family and friends.
+  Yes, I have no experience in real projects, but there is a great desire to get it.
+      I do not pretend to a high salary at the beginning of my journey.
+      Just give me an interesting job and a new experience.
+      Thanks for attention.`
+ 
+  },
+  'ru': {
+    'name': 'Мартиненко Євген',
+    'experience': `активні продажі непромислова группа,
+                оцінка закладного майна(ломбард),
+    власна справа(закупка, продаж обладнання для важкої промисловості).`,
+    'exp': 'Досвід роботи:',
+    'des': 'Навички і вміння:',
+    'ed': 'Освіта:',
+    'edvalue': 'Банківська справа. Бакалавр. Українська Академія Банківської справи.',
+    'loc': 'Черкаси, Україна',
+    'location': 'Місцезнаходження',
+    'courses': 'Курси',
+    'phone': 'Teл.:',
+    'letter': 'Супровідний лист',
+    'coletter': `Вітаю, мене звуть Євген, мені 39 років.
+      Я твердо вирішив змінити рід своєї діяльності, тому що вважаю сферу розробки в IT дуже привабливою та цікавою.
+      Я 
+
+пройшов не маленький життєвий шлях і працював у різних сферах.У деяких я починав з нуля не розуміючи, наприклад
+      як влаштований ліфт чи екскаватор чи мостовий 
+
+кран чи пожежний промисловий насос і що їх може поєднувати.
+      Але завдяки тому, що мені було це цікаво і я отримував задоволення від того, що вирішував здавалося б 
+
+нерозв'язні завдання.
+      я досягав успіхів.
+      Мені навіть вдалося організувати свою невелику справу в якій я був і директор та менеджер з продажу та організатор
+   
+
+   логістики
+      та бухгалтер та водій з вантажником в одній особі.Досягши певного рівня я зрозумів що я настільки все налагодив що
+      стало вже нецікаво
+      
+
+працювати тому, що не надходило нових завдань для рішень.Все йшло гладко, азарту вже не було такого.
+      Чому я вибрав ІТ?
+      Тому що я реально дивлюся на речі та 
+
+розумію, що за цим майбутнє.
+      Я спробував та мені сподобалося та знову з'явився азарт котрий мене рік вже не відпускає.
+      Мені важко було вчитися і працювати на 
+
+двох роботах, а в останні дні ще й у становищі, коли в моїй країні йде війна.
+      Але я не здаюсь.Мені 39, але в душі мені 20. Я займаюся спортом і веду здоровий 
+
+образ життя.Я сповнений енергії та оптимізму.
+      Я займаюся духовними практиками для того, щоб стати кращим і кориснішим для цього світу.
+      Чесність,
+
+    розсудливість, толерантність, комунікабельність, вміння чекати, вміння пристосовуватися якості за які
+      мене цінують мої колеги,
+    сім'я та друзі.
+      Так, у 
+
+мене немає досвіду в реальних проектах, але є величезне бажання його отримати.
+      Я не претендую на високу зарплату на початку мого шляху.
+      Просто дайте мені 
+
+цікаву роботу та новий досвід.
+      Дякую за увагу.
+`
+  },
+  'uk': {
+    'coletter': `Здравствуйте, меня зовут Евгений, мне 39 лет.
+Я твердо решил изменить род своей деятельности так как считаю сферу разработки в IT очень привлекательной и интересной.
+Я прошел не маленьки жизненный путь и работал в разных сферах. В некоторых я начинал с нуля не понимая например 
+как устроен лифт или экскаватор или мостовой кран или пожарный промышленный насос и что их может обьединять.
+Но благодаря тому что мне было это интересно и я получал удовольствие от того что решал казалось бы неразрешимые задачи я дочтигал успехов.
+Мне даже удалось организовать свое небольшое дело в котором я был и директор и менеджер по продажам и организатор логистики
+и бухгалтер и водитель сгрузчиком в одном лице. Достигнув определенного уровня я понял что я настолько все наладил что стало уже неинтересно
+работать потому что не поступало новых задач для решений. Все шло гладко, азарта не было уже такого.
+Почему я выбрал IT?
+Потому что я реально смотрю на вещи и понимаю что за этим будущее.
+Я попробовал и мне понравилось и снова появился азарт который меня год уже не отпускает.
+Мне тяжело было учиться и работать на двух работах а в последние дни еще и в положении когда в моей стране идет война.
+Но я не сдаюсь. Мне 39 но в душе мне 20. Я занимаюсь спортом и веду здоровый образ жини. Я полон энергии и оптимизма.
+Я занимаюсь духовными практиками для того что бы стать лучше и полезнее для этого мира.
+Честность, здравомыслие, толерантность, комуникабельность, умение ждать, умение приспосабливаться-те качества за которые меня ценят мои коллеги,
+семья и друзья.
+Да у меня нет опыта в реальных проэктах но есть огромное желание его получить.
+Я не претендую на высокую зарплату в начале моего пути.
+Просто дайте мне интересную работу.`
+  }
+}
+
+$(function() {
+  $('.translate').click(function() {
+    var lang = $(this).attr('id');
+
+    $('.lang').each(function(index, item) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    });
+  });
+});
